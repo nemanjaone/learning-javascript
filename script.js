e1a665faa07aac7c120c7787b17d3164117d598d
Nemanja Blagojevic <zzirnemus@gmail.com>
2021-05-17 18:20:56 -0400

    more js

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ console.log("Hello!");
 
 //let rating = 3;
 
-let rating = 3;
+let rating = 52;
 
-if (rating === 3) {
+if (rating === 2) {
     console.log("Facts!");
+}
+
+else if (rating === 3) {
+    console.log("True!")
+}
+
+else if (rating === 5) {
+    console.log("not right");
+}
+
+else {
+    console.log("lmao");
 };
 
 console.log("Goodbye!");
