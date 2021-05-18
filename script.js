31277f0e691742fc1009fe196ffea633c095bbfa
Nemanja Blagojevic <zzirnemus@gmail.com>
2021-05-18 10:48:37 -0400

    age logic practice

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 console.log("Hello!");
 
-let rating = 52;
+const age = 43;
 
-if (rating === 2) {
-    console.log("Facts!");
-}
-
-else if (rating === 3) {
-    console.log("True!");
-}
-
-else if (rating === 5) {
-    console.log("not right");
-}
-
-else {
-    console.log("lmao");
-};
-
-if (1 || 2 === 2) {
-    console.log("logical");
+if (age > 0 && age < 5) {
+    console.log("FREE");
+} else if (age > 5 && age < 50) {
+    console.log("10$");
+} else if (age > 50 && age < 65) {
+    console.log("20$");
+} else if (age > 65) {
+    console.log("FREE");
+} else {
+    console.log("Error Age!");
 }
 
 console.log("Goodbye!");
