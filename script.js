775be7d947b368717a1027636071a3fbe8e59522
Nemanja Blagojevic <zzirnemus@gmail.com>
2021-05-18 11:00:47 -0400

    switch statement practice

--- script.js
+++ script.js
@@ -14,4 +14,25 @@ if (age > 0 && age < 5) {
     console.log("Error Age!");
 }
 
+//Switch Statement
+const day = 2;
+
+switch (day) {
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+}
+
 console.log("Goodbye!");
