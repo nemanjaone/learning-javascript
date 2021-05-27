2fe0a4ab83d79bcdcf02a2121d8542ffb4f0659a
Nemanja Blagojevic <zzirnemus@gmail.com>
2021-05-27 15:53:17 -0400

    for in loops, iterating over objects

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-//For...Of loops
-const subreddits = ['cringe', 'books', 'chickens', 'destinythegame', 'funny'];
-
-for (let i = 0; i < subreddits.length; i++) {
-    console.log(`Visit reddit.com/r/${subreddits[i]}`);
+//for in loop
+const testScores = {
+    keenan: 95,
+    damon: 50,
+    lezli: 36,
+    Jason: 15
 }
 
-for (let sub of subreddits) {
-    console.log(sub);
+for (let person in testScores) {
+    console.log(`${person} scored ${testScores[person]}`);
 }
+
+let total = 0;
+let scores = Object.values(testScores);
+for (let score of scores) {
+    total += score;
+}
+
+console.log(total / scores.length);
