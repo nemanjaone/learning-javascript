06ba526ef7afb78d3e6ca96574336674c365ad99
Nemanja Blagojevic <zzirnemus@gmail.com>
2021-05-27 15:28:27 -0400

    for of loops

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
-//Guessing Numbers Game
-let maximum = parseInt(prompt("Enter the maximum number!"));
-while (!maximum) {
-    maximum = parseInt(prompt("Enter a valid number!"));
+//For...Of loops
+const subreddits = ['cringe', 'books', 'chickens', 'destinythegame', 'funny'];
+
+for (let i = 0; i < subreddits.length; i++) {
+    console.log(`Visit reddit.com/r/${subreddits[i]}`);
 }
 
-const targetNum = Math.floor(Math.random() * maximum) + 1;
-console.log(targetNum);
-
-let guess = parseInt(prompt("Enter your first guess!"));
-let attempts = 1;
-
-while (parseInt(guess) !== targetNum) {
-    if (guess === 'q') {
-        break;
-    }
-    attempts++;
-    if (guess > targetNum) {
-        guess = prompt("Too high! Enter a new guess:");
-    } else {
-        guess = prompt("Too low! Enter a new guess:");
-    }
-}
-
-if (guess === 'q') {
-    console.log("OK, YOU QUIT!");
-} else {
-    console.log(`You got it, it took you ${attempts} guesses`);
+for (let sub of subreddits) {
+    console.log(sub);
 }
