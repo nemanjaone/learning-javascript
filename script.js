1a77f9eb7f28d6a7160727dc4020136ca9305da2
Nemanja Blagojevic <zzirnemus@gmail.com>
2021-05-27 16:25:52 -0400

    Todo List in the console

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-//for in loop
-const testScores = {
-    keenan: 95,
-    damon: 50,
-    lezli: 36,
-    Jason: 15
-}
+//Todo List in the Console
+let userInput = prompt("What would you like to do?");
+let todoList = [];
 
-for (let person in testScores) {
-    console.log(`${person} scored ${testScores[person]}`);
+while (userInput != 'quit') {
+    if (userInput === 'new') {
+        let newTodo = prompt("Input new item todo");
+        todoList.push(newTodo);
+    } else if (userInput === 'list') {
+        console.log("**********")
+        for (let [ind, item] of todoList.entries()) {
+            console.log(`${ind}: ${item}`);
+        }
+        console.log("**********")
+    } else if (userInput === 'delete') {
+        let deleteInd = prompt("Index of item for deletion?");
+        todoList.splice(deleteInd, 1);
+    } else if (userInput === 'erase') {
+        let areYouSure = prompt("Are you sure - Delete All (y/n)?");
+        if (areYouSure === 'y') {
+            todoList = [];
+        } else {
+            break;
+        }
+    }
+    userInput = prompt("What would you like to do?");
 }
-
-let total = 0;
-let scores = Object.values(testScores);
-for (let score of scores) {
-    total += score;
-}
-
-console.log(total / scores.length);
