4707534c73503d72ce3c13e8cd6a030d060019bc
Nemanja Blagojevic <zzirnemus@gmail.com>
2022-05-18 12:46:01 -0400

    learning JS promises

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-//Todo List in the Console
-let userInput = prompt("What would you like to do?");
-let todoList = [];
-
-while (userInput != 'quit') {
-    if (userInput === 'new') {
-        let newTodo = prompt("Input new item todo");
-        todoList.push(newTodo);
-    } else if (userInput === 'list') {
-        console.log("**********")
-        for (let [ind, item] of todoList.entries()) {
-            console.log(`${ind}: ${item}`);
-        }
-        console.log("**********")
-    } else if (userInput === 'delete') {
-        let deleteInd = prompt("Index of item for deletion?");
-        todoList.splice(deleteInd, 1);
-    } else if (userInput === 'erase') {
-        let areYouSure = prompt("Are you sure - Delete All (y/n)?");
-        if (areYouSure === 'y') {
-            todoList = [];
-        } else {
-            break;
-        }
-    }
-    userInput = prompt("What would you like to do?");
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        const rand = Math.random();
+        setTimeout(() => {
+            if (rand < 0.7) {
+                resolve('Your Fake Data');
+            }
+            reject('Error');
+        }, 1000)
+    })
 }
+
+fakeRequest('/dogs/1')
+    .then((data) => {
+        console.log("Done with Request");
+        console.log('data is:', data);
+    })
+    .catch((err) => {
+        console.log("Oh no!", err);
+    });
