0e1b57674844497232964b5f94bd9049d6ff3349
Nemanja Blagojevic <zzirnemus@gmail.com>
2022-05-19 12:35:28 -0400

    random starwars character through API

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const loadStarWarsPeople = async () => {
+
+const btn = document.querySelector('button');
+const person = document.querySelector('#person');
+
+
+const getStarWarsPerson = async () => {
+    let random = Math.floor(Math.random() * 20);
+    console.log(random);
     try {
-        const res = await fetch("https://swapi.dev/api/people/1/");
-        const data = await res.json();
-        console.log(data);
+        const res = await axios.get(`https://swapi.dev/api/people/${random}/`);
+        console.log(res.data);
+        person.innerHTML = res.data.name;
     }
     catch (e) {
         console.log(e);
     }
-}
+};
 
-loadStarWarsPeople();
+
+
+btn.addEventListener('click', getStarWarsPerson);
