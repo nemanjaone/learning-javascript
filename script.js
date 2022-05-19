b09bbd4ac51719e8e31dfd0f092358f7df51cb4b
Nemanja Blagojevic <zzirnemus@gmail.com>
2022-05-19 11:39:00 -0400

    fetch api with async await

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const data = `{"ticker":{"base":"BTC","target":"USD","price":"29771.54668105","volume":"31413.67301335","change":"75.51235769"},"timestamp":1652971505,"success":true,"error":""}`
+const loadStarWarsPeople = async () => {
+    try {
+        const res = await fetch("https://swapi.dev/api/people/1/");
+        const data = await res.json();
+        console.log(data);
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
 
-const parsedData = JSON.parse(data); //convert JSON to JS object
-
-console.log(parsedData.ticker.price); //BTC price 
-
-const dataStringify = JSON.stringify(parsedData); // convert JS object to JSON
-
-console.log(dataStringify);
+loadStarWarsPeople();
